4afd09a163315b10934ff61769c6ec5303b688b6
Clementine <your@email.com>
2022-01-26 12:19:02 +0100

    add README

--- src/Result.jsx
+++ src/Result.jsx
@@ -18,22 +18,30 @@ export default function Result({ result, showSnack }) {
   return (
     <div className="container">
       <h1>Result</h1>
-      <div className="result-display">
+      <div className="result-container">
         <div className="result-item">
-          <input
+          <h2>Short Selector</h2>
+          <textarea
+            rows={4}
+            cols={100}
             type="text"
-            className="input-result"
-            readOnly
+            name="full-selector"
+            className="output-result"
             value={shortSelector}
+            readOnly
           />
           <button onClick={(e) => copyOnClipboard(shortSelector)}>Copy</button>
         </div>
         <div className="result-item">
-          <input
+          <h2> JS Selector</h2>
+          <textarea
+            rows={4}
+            cols={100}
             type="text"
-            className="input-result"
-            readOnly
+            name="full-selector"
+            className="output-result"
             value={querySelector}
+            readOnly
           />
           <button onClick={(e) => copyOnClipboard(querySelector)}>Copy</button>
         </div>
